72490e5bcdd4da6c8c5b5a476964f41bd712ca6d
Martin Mendoza <martinmendozadev@gmail.com>
2026-01-07 01:50:45 -0600

    Add: Channel and Message models with associations and validations; update User model associations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :channel
-  belongs_to :parent
+
+  belongs_to :parent, class_name: "Message", optional: true
+  has_many :replies, class_name: "Message", foreign_key: "parent_id", dependent: :destroy
+
+  validates :content, presence: true
 end
