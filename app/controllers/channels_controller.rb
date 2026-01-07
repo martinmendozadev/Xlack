d2486c6d62beb31700f9d7c18dc2f786f55da937
Martin Mendoza <martinmendozadev@gmail.com>
2026-01-07 02:07:42 -0600

    Add: implement channel index view with messages and user session details

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,4 +1,16 @@
 class ChannelsController < ApplicationController
   def index
+    @channels = current_user.channels
+
+    @active_channel = @channels.first
+
+    if @active_channel
+      @messages = @active_channel.messages
+                                 .where(parent_id: nil)
+                                 .includes(:user)
+                                 .order(created_at: :asc)
+    else
+      @messages = []
+    end
   end
 end
