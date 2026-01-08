f6044e9cc75e57e80efdc53cbaaf2cbd18451d3a
Martin Mendoza <martinmendozadev@gmail.com>
2026-01-08 02:22:20 -0600

    Update: refactor ChannelsController and views for improved channel handling and localization

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -1,16 +1,33 @@
 class ChannelsController < ApplicationController
   def index
-    @channels = current_user.channels
+    first_channel = current_user.channels.first
 
-    @active_channel = @channels.first
-
-    if @active_channel
-      @messages = @active_channel.messages
-                                 .where(parent_id: nil)
-                                 .includes(:user)
-                                 .order(created_at: :asc)
+    if first_channel
+      redirect_to channel_path(first_channel)
     else
-      @messages = []
+      render plain: t('channels.no_active_channel')
     end
+  end
+
+  def show
+    @channels = current_user.channels
+    begin
+      @active_channel = Channel.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path, alert: t('channels.channel_not_found')
+      return
+    end
+
+    unless @channels.include?(@active_channel)
+      redirect_to root_path, alert: t('channels.no_access_to_channel')
+      return
+    end
+
+    @messages = @active_channel.messages
+                               .where(parent_id: nil)
+                               .includes(:user)
+                               .order(created_at: :asc)
+
+    render :index
   end
 end
