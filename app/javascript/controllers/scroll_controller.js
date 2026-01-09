58bba03e4e59a3ad89c763e05c5c2247b6ef5371
Martin Mendoza <martinmendozadev@gmail.com>
2026-01-09 01:03:49 -0600

    Add: implement scroll controller for automatic message scrolling in chat

---
+++ app/javascript/controllers/scroll_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    console.log("Scroll controller connected")
+    this.scrollToBottom()
+
+    this.observer = new MutationObserver(() => {
+      this.scrollToBottom()
+    })
+
+    this.observer.observe(this.element, {
+      childList: true,
+      subtree: true
+    })
+  }
+
+  disconnect() {
+    this.observer.disconnect()
+  }
+
+  scrollToBottom() {
+    this.element.scrollTop = this.element.scrollHeight
+  }
+}
